a30f1b2a444ac944b38a226e7c44630f1a796e3d
rafael <rafaelmarinhoalves5@gmail.com>
2024-11-29 20:55:47 -0300

    first commit

---
+++ arenapro/src/test/java/com/web2/arenapro/ArenaproApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web2.arenapro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArenaproApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
