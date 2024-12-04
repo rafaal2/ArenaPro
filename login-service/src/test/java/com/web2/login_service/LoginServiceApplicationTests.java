e4c7cb19d42817746c3151afce39df41d0e84bba
Kaio Vinícius <kaiovsbarbosa@gmail.com>
2024-12-04 19:42:25 -0300

    feat: arruma arquitetura

---
+++ login-service/src/test/java/com/web2/login_service/LoginServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web2.login_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
