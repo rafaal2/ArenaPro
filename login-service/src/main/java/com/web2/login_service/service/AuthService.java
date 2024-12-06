a75af0e0cedb3bc9798c47abb7187146bafff987
Kaio Vinícius <kaiovsbarbosa@gmail.com>
2024-12-05 21:33:07 -0300

    Init project

---
+++ login-service/src/main/java/com/web2/login_service/service/AuthService.java
@@ -0,0 +1,19 @@
+package com.web2.login_service.service;
+
+import com.web2.arenapro.domain.entities.Usuario;
+import com.web2.login_service.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthService {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    public boolean authenticate(String email, String senha) {
+        return usuarioRepository.findByEmail(email)
+                .map(usuario -> usuario.getSenha().equals(senha))
+                .orElse(false);
+    }
+}
