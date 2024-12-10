5a64bf4eb1a9ef69e1857e41dd724e790788f71b
Kaio Vinícius <kaiovsbarbosa@gmail.com>
2024-12-10 18:30:35 -0300

    hotfix: entity scan do login-service

--- login-service/src/main/java/com/web2/login_service/LoginServiceApplication.java
+++ login-service/src/main/java/com/web2/login_service/LoginServiceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
-@EntityScan(basePackages = "com.web2.arenapro.domain.entities")
+@EntityScan(basePackages = "com.web2.login_service.entities")
 public class LoginServiceApplication {
 
 	public static void main(String[] args) {
